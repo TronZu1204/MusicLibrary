e5dbe8100df38052ac23bef08670742ea6932725
Dương Đức Khải <greenjohnny135@gmail.com>
2023-11-16 17:29:41 +0700

    add functions to addmusictoPlaylist

--- src/main/java/DBUtil/PlaylistDB.java
+++ src/main/java/DBUtil/PlaylistDB.java
@@ -3,83 +3,101 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DBUtil;
-
+import java.util.Set;
+import java.util.HashSet;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import LibraryClass.Playlist;
 import LibraryClass.User;
+import LibraryClass.Music;
 import java.util.List;
-import javax.persistence.NoResultException; 
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
+
 /**
  *
  * @author GIGABYTE
  */
 public class PlaylistDB {
-    public static void addPlaylist(Playlist playlist,long user){
+
+    public static void addPlaylist(Playlist playlist, long user) {
         EntityManager em = DButil.getFactory().createEntityManager();
         EntityTransaction trans = em.getTransaction();
-        User owner = em.find(User.class,user);
+        User owner = em.find(User.class, user);
         playlist.setUser(owner);
         trans.begin();
         try {
             em.persist(playlist);
             trans.commit();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
             trans.rollback();
-        }
-        finally {
+        } finally {
             em.close();
         }
     }
-    
-    public static List<Playlist> selectPlaylist(User userID){
-         EntityManager em = DButil.getFactory().createEntityManager();
-    String qString = "Select u FROM Playlist u " + "WHERE u.user = :id";
-    TypedQuery<Playlist> q = em.createQuery(qString, Playlist.class);
-    q.setParameter("id", userID);
-    List<Playlist> playlist = null;
-    try{
-        playlist = q.getResultList();
-        return playlist;
-    }
-    catch (NoResultException e){
-        return null;
-    }
-    finally{
+
+    public static void addSongsToPlaylist(Playlist playlistID, Set<Music> addedSongs) {
+        EntityManager em = DButil.getFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        trans.begin();
+        Playlist playlist = em.find(Playlist.class, playlistID.getPlaylistID());
+        try {
+            Set<Music> oldSongs = playlist.getSongs();
+            Set<Music> newSongs = new HashSet<Music>(oldSongs);
+            newSongs.addAll(addedSongs);
+            playlist.setSongs(newSongs);
+            em.merge(playlist);
+            trans.commit();
+        } catch (Exception e) {
+            System.out.println(e);
+            trans.rollback();
+        }
         em.close();
-    }  
     }
-    public static void deletePlaylist(long playlistID){
+
+    public static List<Playlist> selectPlaylist(User userID) {
+        EntityManager em = DButil.getFactory().createEntityManager();
+        String qString = "Select u FROM Playlist u " + "WHERE u.user = :id";
+        TypedQuery<Playlist> q = em.createQuery(qString, Playlist.class);
+        q.setParameter("id", userID);
+        List<Playlist> playlist = null;
+        try {
+            playlist = q.getResultList();
+            return playlist;
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void deletePlaylist(long playlistID) {
         EntityManager em = DButil.getFactory().createEntityManager();
         Playlist removeplaylist = em.find(Playlist.class, playlistID);
         EntityTransaction trans = em.getTransaction();
         trans.begin();
-        try{
+        try {
             em.remove(em.merge(removeplaylist));
             trans.commit();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
-            trans.rollback();     
-        }
-        finally {
+            trans.rollback();
+        } finally {
             em.close();
         }
     }
-    public static void updatePlaylist(Playlist newPlaylist){
-         EntityManager em = DButil.getFactory().createEntityManager();
+
+    public static void updatePlaylist(Playlist newPlaylist) {
+        EntityManager em = DButil.getFactory().createEntityManager();
         EntityTransaction trans = em.getTransaction();
         trans.begin();
         Playlist oldPlaylist = em.find(Playlist.class, newPlaylist.getPlaylistID());
-        try{
+        try {
             oldPlaylist.setName(newPlaylist.getName());
-             em.merge(oldPlaylist);
+            em.merge(oldPlaylist);
             trans.commit();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
             trans.rollback();
         }
--- src/main/java/Webservlet/PlaylistServlet.java
+++ src/main/java/Webservlet/PlaylistServlet.java
@@ -10,85 +10,88 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.text.SimpleDateFormat;  
-import java.util.Date;  
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import LibraryClass.Playlist;
+import LibraryClass.Music;
 import DBUtil.PlaylistDB;
 import LibraryClass.User;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
 /**
  *
  * @author GIGABYTE
  */
 public class PlaylistServlet extends HttpServlet {
 
-
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         User user = (User) request.getSession().getAttribute("loggeduser");
-       long ID = user.getUserID();
-       List<Playlist> playlist = PlaylistDB.selectPlaylist(user);
-        String url ="/Playlist.jsp";
-       request.setAttribute("playlist", playlist);
-       String action = request.getParameter("action");
+        long ID = user.getUserID();
+        List<Playlist> playlist = PlaylistDB.selectPlaylist(user);
+        String url = "/Playlist.jsp";
+        request.setAttribute("playlist", playlist);
+        String action = request.getParameter("action");
         System.out.println(action);
-       if(action.equals("addPlaylist")){
-           createPlaylist(request,response,ID);
-           String message ="Playlist added";
-           request.setAttribute("message", message);
-           playlist = PlaylistDB.selectPlaylist(user);
-           request.setAttribute("playlist", playlist);
-           url ="/Playlist.jsp";
-       }
-       if(action.equals("Delete playlist")){
-           deletePlaylist(request,response);
-           String message ="Playlist deleted";
-           request.setAttribute("message", message);
-           playlist = PlaylistDB.selectPlaylist(user);
-           request.setAttribute("playlist", playlist);
-           url ="/Playlist.jsp";
-       }
-        if(action.equals("Rename playlist")){
-            updatePlaylist(request,response);
-              String message ="Playlist renamed";
-           request.setAttribute("message", message);
-           playlist = PlaylistDB.selectPlaylist(user);
-           request.setAttribute("playlist", playlist);
-           url ="/Playlist.jsp";
+        if (action.equals("addPlaylist")) {
+            createPlaylist(request, response, ID);
+            String message = "Playlist added";
+            request.setAttribute("message", message);
+            playlist = PlaylistDB.selectPlaylist(user);
+            request.setAttribute("playlist", playlist);
+            url = "/Playlist.jsp";
         }
-          getServletContext()
+        if (action.equals("Delete playlist")) {
+            deletePlaylist(request, response);
+            String message = "Playlist deleted";
+            request.setAttribute("message", message);
+            playlist = PlaylistDB.selectPlaylist(user);
+            request.setAttribute("playlist", playlist);
+            url = "/Playlist.jsp";
+        }
+        if (action.equals("Rename playlist")) {
+            updatePlaylist(request, response);
+            String message = "Playlist renamed";
+            request.setAttribute("message", message);
+            playlist = PlaylistDB.selectPlaylist(user);
+            request.setAttribute("playlist", playlist);
+            url = "/Playlist.jsp";
+        }
+        if (action.equals("Add songs to playlist"))
+        
+        getServletContext()
                 .getRequestDispatcher(url)
-                .forward(request,response);
+                .forward(request, response);
     }
-    
-    private void createPlaylist(HttpServletRequest request, HttpServletResponse response, long ID){
-            String playlistName = request.getParameter("playlistName");
-            long millis=System.currentTimeMillis();  
-            java.sql.Date date=new java.sql.Date(millis);  
-           Playlist playlist = new Playlist();
-           playlist.setCreated(date);
-           playlist.setName(playlistName);
-           playlist.setCreated(date);
-           PlaylistDB.addPlaylist(playlist, ID);
+
+    private void createPlaylist(HttpServletRequest request, HttpServletResponse response, long ID) {
+        String playlistName = request.getParameter("playlistName");
+        long millis = System.currentTimeMillis();
+        java.sql.Date date = new java.sql.Date(millis);
+        Playlist playlist = new Playlist();
+        playlist.setCreated(date);
+        playlist.setName(playlistName);
+        playlist.setCreated(date);
+        PlaylistDB.addPlaylist(playlist, ID);
     }
-    private void deletePlaylist(HttpServletRequest request, HttpServletResponse response){
-           String ID = request.getParameter("playlistID");
-           long playlistID = Long.parseLong(ID);
-           PlaylistDB.deletePlaylist(playlistID);
+
+    private void deletePlaylist(HttpServletRequest request, HttpServletResponse response) {
+        String ID = request.getParameter("playlistID");
+        long playlistID = Long.parseLong(ID);
+        PlaylistDB.deletePlaylist(playlistID);
     }
-    private void updatePlaylist(HttpServletRequest request, HttpServletResponse response){
+
+    private void updatePlaylist(HttpServletRequest request, HttpServletResponse response) {
         String ID = request.getParameter("playlistID");
         long playlistID = Long.parseLong(ID);
         String Name = request.getParameter("renamePlaylist");
@@ -96,5 +99,20 @@ public class PlaylistServlet extends HttpServlet {
         playlist.setName(Name);
         playlist.setPlaylistID(playlistID);
         PlaylistDB.updatePlaylist(playlist);
+    }
+
+    private void addSongsToPlaylist(long playlistID, Set<Long> songIDs) {
+        Playlist playlist = new Playlist();
+        playlist.setPlaylistID(playlistID);
+
+        Set<Music> songs = new HashSet<Music>();
+
+        for (Long songID : songIDs) {
+            Music song = new Music();
+            song.setMusicID(songID);
+            songs.add(song);
+        }
+
+        PlaylistDB.addSongsToPlaylist(playlist, songs);
     }
 }
